988faa8a13c1e006bebcd82d6e11447ab92113a6
afzalimamias <afzalimamias@gmail.com>
2022-07-23 19:28:43 +0530

    fixes some bugs

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/api/comments', (req, res, next) => {
     });
 })
 
-const server = app.listen(3000, () => {
+const server = app.listen( process.env.PORT || 3000, () => {
     console.log("Server is running on port 3000");
 });
 
